e62f51d41bbccc651aa99dce2976244abc4baa76
Daniel Baughman <danielrbaughman@icloud.com>
2025-10-25 09:46:55 -0400

    prettier

--- playground.ts
+++ playground.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 import process from "node:process";
 import { Command } from "commander";
-import { Airtable } from "./output"
+import { Airtable } from "./output";
 
 const program = new Command();
 program.action(async () => {
-  const companies = await new Airtable().companies.get();
+	const companies = await new Airtable().companies.get();
 	console.log(companies.length);
 });
 
