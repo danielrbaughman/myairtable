031f6277f530b90f024cb8a84f89c36bee5a5c3e
Daniel Baughman <danielrbaughman@icloud.com>
2025-10-25 10:09:50 -0400

    notes

--- main.py
+++ main.py
@@ -5,21 +5,32 @@ from typer import Argument, Option, Typer
 from src.csv import gen_csv
 from src.meta import gen_meta, get_base_id, get_base_meta_data
 from src.python import gen_python
-from src.typescript import gen_typescript
 
-# from src import csv, meta, python, typescript
+# from src.typescript import gen_typescript
 
 # TODO
-# - Add option for custom naming of tables/models
-# - need to handle or at least detect duplicate property names
+# Basics
+# - migration from old repo
+# - remove from old repo
+# - output to old repo
+
+# General Improvements
 # - improve property naming
+# - need to handle or at least detect duplicate property names
+# - Add option for custom naming of tables/models
+
+# CLI
 # - improve invalid field message
-# - improve the CLI with lots of options to enable/disable features
+# - improve the CLI with lots of options to enable/disable features (python only for now)
 
 # TS
 # - Need to see what happens to read/write when the field name changes
+# - Migrate to Airtable-TS
 # - Use Zod for type validation?
-# - Migrate to AirtableTS, or learn from it - especially the error handling
+# - improve the CLI with lots of options to enable/disable features (typescript)
+
+# JS ???
+
 
 app = Typer()
 
@@ -53,14 +64,14 @@ def py(folder: str = Argument(help="Path to the output folder"), verbose: bool =
     gen_python(metadata, base_id, verbose, folder=folder_path)
 
 
-@app.command()
-def ts(folder: str = Argument(help="Path to the output folder"), verbose: bool = False):
-    """Generate types and models in TypeScript"""
-    base_id = get_base_id()
-    metadata = get_base_meta_data(base_id)
-    folder_path = Path(folder)
-    folder_path.mkdir(parents=True, exist_ok=True)
-    gen_typescript(metadata, base_id, verbose, folder=folder_path)
+# @app.command()
+# def ts(folder: str = Argument(help="Path to the output folder"), verbose: bool = False):
+#     """Generate types and models in TypeScript"""
+#     base_id = get_base_id()
+#     metadata = get_base_meta_data(base_id)
+#     folder_path = Path(folder)
+#     folder_path.mkdir(parents=True, exist_ok=True)
+#     gen_typescript(metadata, base_id, verbose, folder=folder_path)
 
 
 if __name__ == "__main__":
