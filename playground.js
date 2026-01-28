a8b5c48662f97fe18fefbe00f2ab18e3759e8ef9
Daniel Baughman <danielrbaughman@icloud.com>
2026-01-28 14:10:56 -0500

    code clean

--- playground.js
+++ playground.js
@@ -6,8 +6,6 @@ async function main() {
 	console.log("Hello World (JavaScript)");
 
 	const airtable = new Airtable();
-	const job = await airtable.table("Jobs").get("recHKg9mlUN0jiwOu");
-	console.log(job.name);
 }
 
 main().catch((err) => {
